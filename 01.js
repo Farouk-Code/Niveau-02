8eddddae1a4dbf0e04de5e32053464c47c0c409d
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-02-23 21:56:54 +0100

    Exeercice combinaison de 3 nombres v2

--- 01.js
+++ 01.js
@@ -1,11 +1,13 @@
 function combination() {
+    let result = '';
     for (let i = 0; i <= 9; i++) {
         for (let j = i + 1; j <= 9; j++) {
             for (let k = j + 1; k <= 9; k++) {
-                console.log(i.toString()+j.toString()+k.toString());
+                result += i.toString() + j.toString() + k.toString() + ', ';
             }
         }
     }
+    return result.slice(0, -2);
 }
 
-combination();
+console.log(combination());
