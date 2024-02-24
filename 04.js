fb82b4703961878b02b3e3bf440df039cf4030b6
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-02-24 16:43:01 +0100

    Suite de fibonacci au n-ème élément

---
+++ 04.js
@@ -0,0 +1,19 @@
+// Suite de Fibonacci
+
+if (process.argv.length != 3) {
+    console.log("-1");
+} else if (isNaN(process.argv[2]) || parseInt(process.argv[2]) < 0) {
+    console.log("-1");
+} else {
+    let element = parseInt(process.argv[2]);
+    // let element = 3;
+    let tableauFibo = [0, 1];
+    let resultatFibo = 0;
+
+    for (let i = 1; i < element; i++) {
+        resultatFibo = tableauFibo[i] + tableauFibo[i - 1];
+        tableauFibo.push(resultatFibo);
+    }
+
+    console.log(tableauFibo[element]);
+}
