360fcb7ba6b6fe8ceffdca942fef194914108464
Farouk <farouk@macbook-pro-de-farouk.home>
2024-02-27 03:27:43 +0100

    passage fr

--- 06_V2.js
+++ 06_V2.js
@@ -15,7 +15,7 @@ function isInTheString(firstString, stringToCompar) {
 }
 
 if (process.argv.length < 4) {
-    console.log("Error");
+    console.log("Erreur");
 } else {
     let firstString = process.argv[2];
     let stringToCompar = process.argv[3];
