18495e79f4fc4f8425393afc27a3be3ba08913cf
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-03-02 03:16:50 +0100

    String dans String mis à niveau

--- 06.js
+++ 06.js
@@ -1,14 +1,47 @@
 // String dans String
 
-if (process.argv.length < 4) {
-    console.log("Error");
-} else {
-    let firstString = process.argv[2];
-    let stringToCompar = process.argv[3];
 
-    if (firstString.includes(stringToCompar)) {
-        console.log("true");
+// FONCTION
+function isInTheString(firstString, stringToCompar) {
+    for (let i = 0; i <= firstString.length - stringToCompar.length; i++) {
+        let correspondence = true
+        for (let j = 0; j < stringToCompar.length; j++) {
+            if (firstString[i + j] !== stringToCompar[j]) {
+                correspondence = false
+                break
+            }
+        }
+        if (correspondence) {
+            return true
+        }
+    }
+    return false
+}
+
+
+// PARSING
+function getArgument() {
+    return process.argv.slice(2)
+}
+
+
+// GESTION DES ERREURS
+function isValidArguments(arguments) {
+    if (arguments.length !== 2) {
+        return console.log("Error")
     } else {
-        console.log("false");
+        return arguments
     }
 }
+
+
+// RESOLUTION
+function getResult() {
+    const arguments = isValidArguments(getArgument())
+    if (!arguments) {return }
+    return console.log(isInTheString(arguments[0], arguments[1]))
+}
+
+
+// AFFICHAGE
+getResult()
