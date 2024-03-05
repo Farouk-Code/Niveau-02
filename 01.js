9529e71a33bb89e409fb3d73ca5800d6cde12236
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-03-06 00:02:29 +0100

    Correction exercice 01

--- 01.js
+++ 01.js
@@ -2,22 +2,28 @@
 
 // FONCTION
 function combination() {
-    const result = []
-    for (let i = 0; i <= 9; i++) {
-        for (let j = i + 1; j <= 9; j++) {
-            for (let k = j + 1; k <= 9; k++) {
-               result.push(i.toString() + j.toString() + k.toString())
-            }
-        }
+  const result = [];
+  for (let i = 0; i <= 9; i++) {
+    for (let j = i + 1; j <= 9; j++) {
+      for (let k = j + 1; k <= 9; k++) {
+        result.push(i.toString() + j.toString() + k.toString());
+      }
     }
-    return result
+  }
+  return result;
 }
 
 // RESOLUTION
-function showTable () {
-    return console.log(combination().join(', '))
+function showTable() {
+  // return console.log(combination().join(', '))
+  let sequence = "";
+  let array = combination();
+  for (element of array) {
+    sequence += element + " ";
+  }
+  return console.log(sequence);
 }
 
 // AFFICHAGE RESULTAT
 
-showTable()
+showTable();
